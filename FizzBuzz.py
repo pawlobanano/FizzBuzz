0f84b9b9dc69921bee8aaed47aaf28066a694cf2
Paweł Banach <pawlobanano@users.noreply.github.com>
2018-05-03 15:58:55 +0200

    2nd iteration of "FizzBuzz"

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
-    if (i % 3 == 0 and i % 5 == 0):
-        print("FizzBuzz")
-    elif (i % 3 == 0):
-        print("Fizz")
-    elif (i % 5 == 0):
-        print("Buzz")
-    else:
+    output = ""
+    if (i % 3 == 0):
+        output += "Fizz"
+    if (i % 5 == 0):
+        output += "Buzz"
+    if(output == ""):
         print(i)
+    print(output)
