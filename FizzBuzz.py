73f60f9707fdaadfb6ed671d55b026f2499975b8
Paweł Banach <pawlobanano@users.noreply.github.com>
2018-05-03 15:59:31 +0200

    3rd iteration of "FizzBuzz"

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,25 @@
+def i_divisible_by(i, num):
+    if (i % num == 0):
+        return True
+    return False
+
+
+def output_printer(str):
+    output = ""
+    return output + str
+
+
+choices = {
+    3: output_printer("Fizz"),
+    5: output_printer("Buzz")
+}
+
 for i in range(1, 101):
     output = ""
-    if (i % 3 == 0):
-        output += "Fizz"
-    if (i % 5 == 0):
-        output += "Buzz"
-    if(output == ""):
-        print(i)
+    if (i_divisible_by(i, 3)):
+        output += choices.get(3)
+    if (i_divisible_by(i, 5)):
+        output += choices.get(5)
+    if (output == ""):
+        output = output_printer(str(i))
     print(output)
